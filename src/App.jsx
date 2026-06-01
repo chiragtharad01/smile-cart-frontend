cfa3427a612e6971c77a342596a71542b2b63861
chirag <chiragtharad01@gmail.com>
2026-06-01 16:07:01 +0530

    Replaced CartItemsContext with useCartItemsStore

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,3 @@
-import { useState } from "react";
-
 import PageNotFound from "components/commons/PageNotFound";
 import Product from "components/Product/index";
 import ProductList from "components/ProductList/index";
@@ -7,33 +5,26 @@ import { NavLink, Route, Switch, Redirect } from "react-router-dom";
 import routes from "routes";
 
 import "./App.css";
-import CartItemsContext from "./contexts/CartItemsContext";
 
 // eslint-disable-next-line import/extensions
 
-const App = () => {
-  const [cartItems, setCartItems] = useState([]);
-
-  return (
-    <>
-      <div className="flex space-x-2">
-        <NavLink exact activeClassName="underline font-bold" to="/products">
-          Home
-        </NavLink>
-        <NavLink exact activeClassName="underline font-bold" to="/product">
-          Product
-        </NavLink>
-      </div>
-      <CartItemsContext.Provider value={[cartItems, setCartItems]}>
-        <Switch>
-          <Route exact component={Product} path={routes.products.show} />
-          <Route exact component={ProductList} path={routes.products.index} />
-          <Redirect exact from={routes.root} to={routes.products.index} />
-          <Route component={PageNotFound} path="*" />
-        </Switch>
-      </CartItemsContext.Provider>
-    </>
-  );
-};
+const App = () => (
+  <>
+    <div className="flex space-x-2">
+      <NavLink exact activeClassName="underline font-bold" to="/products">
+        Home
+      </NavLink>
+      <NavLink exact activeClassName="underline font-bold" to="/product">
+        Product
+      </NavLink>
+    </div>
+    <Switch>
+      <Route exact component={Product} path={routes.products.show} />
+      <Route exact component={ProductList} path={routes.products.index} />
+      <Redirect exact from={routes.root} to={routes.products.index} />
+      <Route component={PageNotFound} path="*" />
+    </Switch>
+  </>
+);
 
 export default App;
