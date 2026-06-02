867915b8b864bafa393e34d5451c05aa7957a9a4
chirag <chiragtharad01@gmail.com>
2026-06-02 14:44:11 +0530

    Added pagination to product listing page

--- src/components/ProductList/index.jsx
+++ src/components/ProductList/index.jsx
@@ -5,20 +5,27 @@ import { useFetchProducts } from "hooks/reactQuery/useProductsApi";
 import useDebounce from "hooks/useDebounce";
 import i18next from "i18next";
 import { Search } from "neetoicons";
-import { Input, NoData } from "neetoui";
+import { Input, NoData, Pagination } from "neetoui";
 import { isEmpty } from "ramda";
 import { useTranslation } from "react-i18next";
 import withTitle from "utils/withTitle";
 
+import { DEFAULT_PAGE_INDEX, DEFAULT_PAGE_SIZE } from "./constants";
 import ProductListItem from "./ProductListItem";
 
 const ProductList = () => {
   const { t } = useTranslation();
+  const [currentPage, setCurrentPage] = useState(DEFAULT_PAGE_INDEX);
   const [searchKey, setSearchKey] = useState("");
   const debouncedSearchKey = useDebounce(searchKey);
-  const { data: { products = [] } = {}, isLoading } = useFetchProducts({
+  const productsParams = {
     searchTerm: debouncedSearchKey,
-  });
+    page: currentPage,
+    pageSize: DEFAULT_PAGE_SIZE,
+  };
+
+  const { data: { products = [], totalProductsCount } = {}, isLoading } =
+    useFetchProducts(productsParams);
 
   if (isLoading) {
     return <PageLoader />;
@@ -35,7 +42,10 @@ const ProductList = () => {
             prefix={<Search />}
             type={t("search")}
             value={searchKey}
-            onChange={event => setSearchKey(event.target.value)}
+            onChange={event => {
+              setSearchKey(event.target.value);
+              setCurrentPage(DEFAULT_PAGE_INDEX);
+            }}
           />
         }
       />
@@ -48,6 +58,14 @@ const ProductList = () => {
           ))}
         </div>
       )}
+      <div className="mb-5 self-end">
+        <Pagination
+          count={totalProductsCount}
+          navigate={page => setCurrentPage(page)}
+          pageNo={currentPage || DEFAULT_PAGE_INDEX}
+          pageSize={DEFAULT_PAGE_SIZE}
+        />
+      </div>
     </div>
   );
 };
